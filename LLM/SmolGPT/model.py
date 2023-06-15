7cd2a5f504ecc462a33203c341586d8c51891932
GaspardCulis <gaspard.culis@gmail.com>
2023-06-15 17:42:44 +0200

    Generate is now iterable

--- LLM/SmolGPT/model.py
+++ LLM/SmolGPT/model.py
@@ -7,8 +7,9 @@ from dataclasses import dataclass
 # T: Time dimension
 # C: Channel dimetion IE vocab_size
 
+
 @dataclass
-class ModelConfig():
+class ModelConfig:
     device: str
     vocab_size: int
     batch_size: int
@@ -18,16 +19,11 @@ class ModelConfig():
     n_layer: int
     dropout: float
 
+
 model_config = ModelConfig(
-    'cuda' if torch.cuda.is_available() else 'cpu',
-    24*2+10,
-    64,
-    256,
-    384,
-    6, 
-    6,
-    0.2
+    "cuda" if torch.cuda.is_available() else "cpu", 24 * 2 + 10, 64, 256, 384, 6, 6, 0.2
 )
+
 
 class SelfAttention(nn.Module):
     def __init__(self, head_size) -> None:
@@ -35,8 +31,11 @@ class SelfAttention(nn.Module):
         self.key = nn.Linear(model_config.n_embd, head_size, bias=False)
         self.query = nn.Linear(model_config.n_embd, head_size, bias=False)
         self.value = nn.Linear(model_config.n_embd, head_size, bias=False)
-        self.register_buffer('tril', torch.tril(torch.ones(model_config.block_size, model_config.block_size)))
-        self.hs_sqrt = head_size ** -0.5
+        self.register_buffer(
+            "tril",
+            torch.tril(torch.ones(model_config.block_size, model_config.block_size)),
+        )
+        self.hs_sqrt = head_size**-0.5
 
         self.dropout = nn.Dropout(model_config.dropout)
 
@@ -45,45 +44,53 @@ class SelfAttention(nn.Module):
         Given a specific context, it will reduce the context to head_size based on its estimations on how important each token of information in the context is.
         """
         _, T, _ = x.shape
-        k = self.key(x) # (B, T, head_size)
-        q = self.query(x) # (B, T, head_size)
+        k = self.key(x)  # (B, T, head_size)
+        q = self.query(x)  # (B, T, head_size)
         v = self.value(x)
-        
-        wei = q @ k.transpose(-2, -1) * self.hs_sqrt # (B, T, 16) @ (B, 16, T) -> (B, T, T)
-        
-        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T), Restricts the access to only the past context (remove for sentiment analysis ?)  # type: ignore
+
+        wei = (
+            q @ k.transpose(-2, -1) * self.hs_sqrt
+        )  # (B, T, 16) @ (B, 16, T) -> (B, T, T)
+
+        wei = wei.masked_fill(
+            self.tril[:T, :T] == 0, float("-inf")
+        )  # (B, T, T), Restricts the access to only the past context (remove for sentiment analysis ?)  # type: ignore
         wei = F.softmax(wei, dim=-1)
         wei = self.dropout(wei)
 
-        return wei @ v # (B, T, T) @ (B, T, C) -> (B, T, C)
-    
+        return wei @ v  # (B, T, T) @ (B, T, C) -> (B, T, C)
+
+
 class MultiSelfAttention(nn.Module):
     def __init__(self, num_heads: int, head_size) -> None:
         super().__init__()
         self.heads = nn.ModuleList([SelfAttention(head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(model_config.n_embd, model_config.n_embd)
         self.dropout = nn.Dropout(model_config.dropout)
-    
+
     def forward(self, x):
         out = torch.cat([h(x) for h in self.heads], dim=-1)
         out = self.dropout(self.proj(out))
         return out
 
+
 class FeedForward(nn.Module):
     """
-    Simple linear layer followed by a non-linearity 
+    Simple linear layer followed by a non-linearity
     """
+
     def __init__(self) -> None:
         super().__init__()
         self.net = nn.Sequential(
             nn.Linear(model_config.n_embd, 4 * model_config.n_embd),
             nn.ReLU(),
             nn.Linear(4 * model_config.n_embd, model_config.n_embd),
-            nn.Dropout(model_config.dropout)
+            nn.Dropout(model_config.dropout),
         )
 
     def forward(self, x):
         return self.net(x)
+
 
 class Block(nn.Module):
     """
@@ -104,53 +111,58 @@ class Block(nn.Module):
         x = x + self.ffwd(self.ln2(x))
         return x
 
-class BigramLanguageModel(nn.Module):
 
+class BigramLanguageModel(nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        self.token_embedding_table = nn.Embedding(model_config.vocab_size, model_config.n_embd)
-        self.position_embedding_table = nn.Embedding(model_config.block_size, model_config.n_embd)
+        self.token_embedding_table = nn.Embedding(
+            model_config.vocab_size, model_config.n_embd
+        )
+        self.position_embedding_table = nn.Embedding(
+            model_config.block_size, model_config.n_embd
+        )
         self.blocks = nn.Sequential(*[Block() for _ in range(model_config.n_layer)])
-        self.ln_f = nn.LayerNorm(model_config.n_embd) # Final layer norm
+        self.ln_f = nn.LayerNorm(model_config.n_embd)  # Final layer norm
         self.lm_head = nn.Linear(model_config.n_embd, model_config.vocab_size)
-        
+
         self.device = model_config.device
         self.ctx_len = model_config.block_size
 
-    def forward(self, inputs: torch.Tensor, targets: torch.Tensor = None): # type: ignore
+    def forward(self, inputs: torch.Tensor, targets: torch.Tensor = None):  # type: ignore
         B, T = inputs.shape
         # inputs and target are both (B, T) tensor of integers
-        tok_emb = self.token_embedding_table(inputs) # (B, T, C)
-        pos_emb = self.position_embedding_table(torch.arange(T, device=self.device)) # (T, C)
-        x = tok_emb + pos_emb # (B, T, C)
-        x = self.blocks(x) # (B, T, C)
-        x = self.ln_f(x) # (B, T, C)
-        logits = self.lm_head(x) # (B, T, vocab_size)
+        tok_emb = self.token_embedding_table(inputs)  # (B, T, C)
+        pos_emb = self.position_embedding_table(
+            torch.arange(T, device=self.device)
+        )  # (T, C)
+        x = tok_emb + pos_emb  # (B, T, C)
+        x = self.blocks(x)  # (B, T, C)
+        x = self.ln_f(x)  # (B, T, C)
+        logits = self.lm_head(x)  # (B, T, vocab_size)
 
         if targets is None:
             loss = None
         else:
             B, T, C = logits.shape
-            logits = logits.view(B*T, C)
-            targets = targets.view(B*T)
+            logits = logits.view(B * T, C)
+            targets = targets.view(B * T)
             loss = F.cross_entropy(logits, targets)
 
         return logits, loss
-    
+
     def generate(self, inputs: torch.Tensor, max_new_tokens: int):
         # inputs (B, T) array of indices in the current context
         for _ in range(max_new_tokens):
             # Crop context to ctx_len
-            inputs_cond = inputs[:, -self.ctx_len:]
+            inputs_cond = inputs[:, -self.ctx_len :]
             # Get predictions
             logits, _ = self(inputs_cond)
             # Focus only on the last timstep
-            logits = logits[:, -1, :] # Becomes (B, C)
+            logits = logits[:, -1, :]  # Becomes (B, C)
             # Apply softmax to get probs
-            probs = F.softmax(logits, dim=-1) # (B, C)
+            probs = F.softmax(logits, dim=-1)  # (B, C)
             # Sample the distribution
-            inputs_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            inputs_next = torch.multinomial(probs, num_samples=1)  # (B, 1)
             # Append the new token to the input context
-            inputs = torch.cat((inputs, inputs_next), dim=1) # (B, T+1)
-        return inputs
-    
+            inputs = torch.cat((inputs, inputs_next), dim=1)  # (B, T+1)
+            yield ([int(inputs[-1][-1])])
--- LLM/SmolGPT/train.py
+++ LLM/SmolGPT/train.py
@@ -2,14 +2,14 @@ import torch
 import utils
 import model
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-block_size = 256 # AKA ctx_len
+device = "cuda" if torch.cuda.is_available() else "cpu"
+block_size = 256  # AKA ctx_len
 batch_size = 32
 save_path = "./SmolGPT.pth"
 
-print("Using "+device+" device.")
+print("Using " + device + " device.")
 
-with open('./dataset/articles.txt', 'r', encoding='utf-8') as f:
+with open("./dataset/articles.txt", "r", encoding="utf-8") as f:
     text = f.read()
 
 print("Lenght of the Dataset:", len(text))
@@ -21,38 +21,33 @@ print(f"Vocab: {tok.vocab}")
 
 data = torch.tensor(tok.encode(text), dtype=torch.long)
 
-n = int(0.9*len(data))
+n = int(0.9 * len(data))
 train_data = data[:n]
 test_data = data[n:]
 
-def get_batch(split = "train"):
+
+def get_batch(split="train"):
     data = train_data if split == "train" else test_data
     ix = torch.randint(len(data) - block_size, (batch_size,))
-    x = torch.stack([data[i:i+block_size] for i in ix])
-    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x = torch.stack([data[i : i + block_size] for i in ix])
+    y = torch.stack([data[i + 1 : i + block_size + 1] for i in ix])
     x, y = x.to(device), y.to(device)
 
     return x, y
 
+
 xb, yb = get_batch()
-print(f'inputs: {xb.shape}')
+print(f"inputs: {xb.shape}")
 print(xb)
-print(f'targets: {yb.shape}')
+print(f"targets: {yb.shape}")
 print(yb)
-print('--------------')
+print("--------------")
 
 model.model_config = model.ModelConfig(
-    device,
-    len(tok.vocab),
-    batch_size,
-    block_size,
-    512,
-    8, 
-    8,
-    0.2
+    device, len(tok.vocab), batch_size, block_size, 512, 8, 8, 0.2
 )
 
-model = torch.load(save_path) #model.BigramLanguageModel()
+model = torch.load(save_path)  # model.BigramLanguageModel()
 m = model.to(device)
 out, loss = model(xb, yb)
 print(out.shape)
@@ -63,7 +58,6 @@ optimizer = torch.optim.AdamW(m.parameters(), lr=1e-4)
 training_steps = 80000
 log_step = 1000
 for step in range(training_steps + 1):
-
     # Sample a batch of training data
     xb, yb = get_batch()
 
@@ -73,16 +67,18 @@ for step in range(training_steps + 1):
     loss.backward()
     optimizer.step()
 
-    if step%1000 == 0:
+    if step % 1000 == 0:
         torch.save(model, save_path)
-        print(f"[{step/log_step}/{training_steps//log_step}] Loss: {loss.item()}   Model saved.")
+        print(
+            f"[{step/log_step}/{training_steps//log_step}] Loss: {loss.item()}   Model saved."
+        )
 
 inputs = torch.tensor([tok.encode("ART")], dtype=torch.long)
 inputs = inputs.to(device)
 print("inputs: ", inputs)
-out = m.generate(inputs, 3200)
-print(out)
+for token in m.generate(inputs, 3200):
+    print(tok.decode(token), end="", flush=True)
 print(tok.decode(out[0].tolist()))
 
 torch.save(model, save_path)
-print("Model saved to "+save_path)
+print("Model saved to " + save_path)
